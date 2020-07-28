346db3aabf34b4e3e410f3b64f97ac3647a3eede
Юрченко Виктор <yur4enko.vitya@yandex.ru>
2020-07-28 16:57:15 +0600

    Update settings.py

--- vk-friends/settings.py
+++ vk-friends/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gw##-!-n#2t!r&3vr^6thl6yen(z*h8d=o9i#-_bq3!jga8i!6'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
